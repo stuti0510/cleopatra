be7857cad5df8e815ec8a9e3b43217e2fbc1807c
ANUPAMA SAROJ <anupamasaroj@ANUPAMAs-MacBook-Air.local>
2025-02-01 23:00:54 +0530

    all

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const authRoutes = require("./routes/auth");
 
 const app = express();
 
-// Middleware
+
 app.use(express.json());
 app.use(cors());
 
-// Database Connection
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.error("MongoDB Connection Error:", err));
 
-// Routes
+
 app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5001;
