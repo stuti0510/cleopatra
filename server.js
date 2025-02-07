b2bf8a9348abea5692f0f47a4b0d15c6bc8a4a4b
ANUPAMA SAROJ <anupamasaroj@ANUPAMAs-MacBook-Air.local>
2025-02-07 20:40:10 +0530

    added

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ const authRoutes = require("./routes/auth");
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
-    methods: ["GET", "POST"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(cors());
 
 app.use(session({
     secret:process.env.SESSION_SECRET,
